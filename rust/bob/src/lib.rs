bd8fb1e2347d7f4fdead8b650b3372cd6e840066
Stephen Wassell <stephen.wassell@sophos.com>
2019-08-22 21:03:32 +0200

    various

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,28 +1,34 @@
 #[macro_use] extern crate lazy_static;
 
-use regex::Regex; // I♥RE
+use regex::RegexSet; // I♥RE
 
 pub fn reply(message: &str) -> &str {
+	
 	lazy_static! {
-		static ref question: Regex = Regex::new(r"\?\s*$").unwrap();
-		static ref contains_uc: Regex = Regex::new(r"[A-Z]").unwrap();
-		static ref contains_lc: Regex = Regex::new(r"[a-z]").unwrap();
-		static ref empty: Regex = Regex::new(r"^\s*$").unwrap();
+		static ref RE: RegexSet = RegexSet::new(&[
+			r"\?\s*$", // ends with a question mark
+			r"[A-Z]",  // contains upper case
+			r"[a-z]",  // contains lower case
+			r"\S"      // not empty
+		]).unwrap();
 	}
 	
-	match (question.is_match(message),
-		contains_uc.is_match(message),
-		contains_lc.is_match(message),
-		empty.is_match(message)) {
-
+	let matches = RE.matches(message);
+	
+	match (
+		matches.matched(0),
+		matches.matched(1),
+		matches.matched(2),
+		matches.matched(3)
+	) {
 		// He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
-		(false, true, false, false) => "Whoa, chill out!",
+		(false, true, false, true) => "Whoa, chill out!",
 		// He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-		(true, true, false, false) => "Calm down, I know what I'm doing!",
+		(true, true, false, true) => "Calm down, I know what I'm doing!",
 		// Bob answers 'Sure.' if you ask him a question, such as "How are you?".
-		(true, _, _, false) => "Sure.",
+		(true, _, _, true) => "Sure.",
 		// He says 'Fine. Be that way!' if you address him without actually saying anything.
-		(_, _, _, true) => "Fine. Be that way!",
+		(_, _, _, false) => "Fine. Be that way!",
 		// He answers 'Whatever.' to anything else.
 		(_, _, _, _) => "Whatever."
 	}
