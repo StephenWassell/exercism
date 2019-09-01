ecf0f1c519b0406dfcfb5f8e86fba81b08c3c587
Stephen Wassell <stephen.wassell@sophos.com>
2019-09-01 21:03:42 +0100

    more

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,32 +1,25 @@
+fn is_question(message: &str) -> bool {
+	message.ends_with('?')
+}
+
+fn is_yelled(message: &str) -> bool {
+    message.chars().any(char::is_uppercase) &&
+    !message.chars().any(char::is_lowercase)
+}
+
 pub fn reply(message: &str) -> &str {
-    let mut contains_upper = false;
-    let mut contains_lower = false;
-    let mut not_empty = false;
-    let mut last = 'x';
+    let trimmed = message.trim();
 
-    for c in message.chars() {
-        if c.is_uppercase() {
-            contains_upper = true;
-        }
-        if c.is_lowercase() {
-            contains_lower = true;
-        }
-        if !c.is_whitespace() {
-            not_empty = true;
-            last = c;
-        }
-    }
-
-    match (last == '?', contains_upper, contains_lower, not_empty) {
+    match (is_question(trimmed), is_yelled(trimmed), trimmed.is_empty()) {
         // He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
-        (false, true, false, true) => "Whoa, chill out!",
+        (false, true, false) => "Whoa, chill out!",
         // He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-        (true, true, false, true) => "Calm down, I know what I'm doing!",
+        (true, true, false) => "Calm down, I know what I'm doing!",
         // Bob answers 'Sure.' if you ask him a question, such as "How are you?".
-        (true, _, _, true) => "Sure.",
+        (true, false, false) => "Sure.",
         // He says 'Fine. Be that way!' if you address him without actually saying anything.
-        (_, _, _, false) => "Fine. Be that way!",
+        (_, _, true) => "Fine. Be that way!",
         // He answers 'Whatever.' to anything else.
-        (_, _, _, _) => "Whatever.",
+        (_, _, _) => "Whatever.",
     }
 }
