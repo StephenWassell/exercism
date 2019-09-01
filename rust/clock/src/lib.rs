653779610a71136c7b1c864008f028207fd8f0dc
Stephen Wassell <stephen.wassell@sophos.com>
2019-09-01 21:01:13 +0000

    clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,36 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    minutes: i32,
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
+    fn normalise(minutes: i32) -> i32 {
+        const MINUTES_IN_DAY: i32 = 60 * 24;
+
+        let minutes = minutes % MINUTES_IN_DAY;
+
+        if minutes >= 0 {
             minutes
-        );
+        } else {
+            MINUTES_IN_DAY + minutes
+        }
+    }
+
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Clock {
+            minutes: Clock::normalise(hours * 60 + minutes),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock {
+            minutes: Clock::normalise(self.minutes + minutes),
+        }
+    }
+}
+
+impl std::fmt::Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }
