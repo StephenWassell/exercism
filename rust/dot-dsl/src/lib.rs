2c080aee0052c8c4ecd1e794f63a8cb67c6e6078
Stephen Wassell <stephen.wassell@sophos.com>
2019-09-14 20:21:55 +0000

    not any more

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -20,10 +20,13 @@ pub mod graph {
         // }
 
         pub mod edge {
+            use std::collections::HashMap;
+
             #[derive(Clone, Debug, PartialEq)]
             pub struct Edge {
                 from: String,
                 to: String,
+                attrs: crate::graph::graph_items::Attrs,
             }
 
             impl Edge {
@@ -31,6 +34,7 @@ pub mod graph {
                     Edge {
                         from: from.to_string(),
                         to: to.to_string(),
+                        attrs: HashMap::new(),
                     }
                 }
 
@@ -38,22 +42,26 @@ pub mod graph {
                     self.clone()
                 }
 
-                pub fn get_attr(&self, name: &str) -> Option<&str> {
-                    None
+                pub fn get_attr(&self, name: &str) -> Option<&String> {
+                    self.attrs.get(name)
                 }
             }
         }
 
         pub mod node {
+            use std::collections::HashMap;
+
             #[derive(Clone, Debug, PartialEq)]
             pub struct Node {
-                name: String,
+                pub name: String,
+                attrs: crate::graph::graph_items::Attrs,
             }
 
             impl Node {
                 pub fn new(name: &str) -> Self {
                     Node {
                         name: name.to_string(),
+                        attrs: HashMap::new(),
                     }
                 }
 
@@ -61,8 +69,8 @@ pub mod graph {
                     self.clone()
                 }
 
-                pub fn get_attr(&self, name: &str) -> Option<&str> {
-                    None
+                pub fn get_attr(&self, name: &str) -> Option<&String> {
+                    self.attrs.get(name)
                 }
             }
         }
@@ -89,25 +97,31 @@ pub mod graph {
         }
 
         pub fn with_nodes(&self, nodes: &[graph_items::node::Node]) -> Self {
-            //self.nodes.extend_from_slice(nodes);
-            self.clone()
+            let mut ret = self.clone();
+            ret.nodes.extend_from_slice(nodes);
+            ret
         }
 
         pub fn with_edges(&self, edges: &[graph_items::edge::Edge]) -> Self {
-            //self.edges.extend_from_slice(edges);
-            self.clone()
+            let mut ret = self.clone();
+            ret.edges.extend_from_slice(edges);
+            ret
         }
 
         pub fn with_attrs(&self, attrs: &[(&str, &str)]) -> Self {
-            self.clone()
+            let mut ret = self.clone();
+            for (name, value) in attrs {
+                ret.attrs.insert(name.to_string(), value.to_string());
+            }
+            ret
         }
 
-        pub fn get_node(&self, name: &str) -> Option<graph_items::node::Node> {
-            None
+        pub fn get_node(&self, name: &str) -> Option<&graph_items::node::Node> {
+            self.nodes.iter().find(|node| node.name == name)
         }
 
-        pub fn get_attr(&self, name: &str) -> Option<&str> {
-            None
+        pub fn get_attr(&self, name: &str) -> Option<&String> {
+            self.attrs.get(name)
         }
     }
 }
