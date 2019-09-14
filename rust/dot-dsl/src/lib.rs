944aca3d6e657294114ecbddb8e480b4117651cd
Stephen Wassell <stephen.wassell@sophos.com>
2019-09-14 19:59:12 +0000

    it compiles!

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,23 +1,23 @@
 pub mod graph {
-	use std::collections::HashMap;
+    use std::collections::HashMap;
 
     pub mod graph_items {
-        pub mod attr {
-            #[derive(Clone, Debug, PartialEq)]
-            pub struct Attr {
-                name: String,
-                value: String,
-            }
+        // pub mod attr {
+        //     #[derive(Clone, Debug, PartialEq)]
+        //     pub struct Attr {
+        //         name: String,
+        //         value: String,
+        //     }
 
-            impl Attr {
-                pub fn new(name: &str, value: &str) -> Self {
-                    Attr {
-                        name: name.to_string(),
-                        value: value.to_string(),
-                    }
-                }
-            }
-        }
+        //     impl Attr {
+        //         pub fn new(name: &str, value: &str) -> Self {
+        //             Attr {
+        //                 name: name.to_string(),
+        //                 value: value.to_string(),
+        //             }
+        //         }
+        //     }
+        // }
 
         pub mod edge {
             #[derive(Clone, Debug, PartialEq)]
@@ -32,6 +32,14 @@ pub mod graph {
                         from: from.to_string(),
                         to: to.to_string(),
                     }
+                }
+
+                pub fn with_attrs(&self, attrs: &[(&str, &str)]) -> Self {
+                    self.clone()
+                }
+
+                pub fn get_attr(&self, name: &str) -> Option<&str> {
+                    None
                 }
             }
         }
@@ -48,14 +56,23 @@ pub mod graph {
                         name: name.to_string(),
                     }
                 }
+
+                pub fn with_attrs(&self, attrs: &[(&str, &str)]) -> Self {
+                    self.clone()
+                }
+
+                pub fn get_attr(&self, name: &str) -> Option<&str> {
+                    None
+                }
             }
-    	}
+        }
 
         pub type Attrs = std::collections::HashMap<String, String>;
         pub type Edges = Vec<edge::Edge>;
         pub type Nodes = Vec<node::Node>;
     }
 
+    #[derive(Clone, Debug)]
     pub struct Graph {
         pub attrs: graph_items::Attrs,
         pub edges: graph_items::Edges,
@@ -71,17 +88,25 @@ pub mod graph {
             }
         }
 
-        pub fn with_nodes(&mut self, nodes: &[graph_items::node::Node]) -> &mut Self {
-            self.nodes.extend_from_slice(nodes);
-            self
+        pub fn with_nodes(&self, nodes: &[graph_items::node::Node]) -> Self {
+            //self.nodes.extend_from_slice(nodes);
+            self.clone()
         }
 
-        pub fn with_edges(&mut self, edges: &[graph_items::edge::Edge]) -> &mut Self {
-            self.edges.extend_from_slice(edges);
-            self
+        pub fn with_edges(&self, edges: &[graph_items::edge::Edge]) -> Self {
+            //self.edges.extend_from_slice(edges);
+            self.clone()
         }
 
-        pub fn get_node(&mut self, name: &str) -> Option<graph_items::node::Node> {
+        pub fn with_attrs(&self, attrs: &[(&str, &str)]) -> Self {
+            self.clone()
+        }
+
+        pub fn get_node(&self, name: &str) -> Option<graph_items::node::Node> {
+            None
+        }
+
+        pub fn get_attr(&self, name: &str) -> Option<&str> {
             None
         }
     }
