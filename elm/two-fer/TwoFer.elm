1e49bd2441c07638955715c4a67c7a3e438e2938
Stephen Wassell <stephen.wassell@sophos.com>
2019-10-09 21:08:11 +0000

    ran elm-format

--- elm/two-fer/TwoFer.elm
+++ elm/two-fer/TwoFer.elm
@@ -4,5 +4,8 @@ module TwoFer exposing (twoFer)
 twoFer : Maybe String -> String
 twoFer name =
     case name of
-        Just(s) -> "One for " ++ s ++ ", one for me."
-        Nothing -> "One for you, one for me."
+        Just s ->
+            "One for " ++ s ++ ", one for me."
+
+        Nothing ->
+            twoFer (Just "you")
