449269c636e79ae51d9c65d87ebc278461c3358b
Stephen Wassell <stephen.wassell@sophos.com>
2019-11-10 19:15:34 +0000

    space-age macro_rules

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,15 +1,6 @@
 // in seconds
 const EARTH_ORBITAL_PERIOD_S: f64 = 31_557_600.0;
 
-// in Earth years
-const MERCURY_ORBITAL_PERIOD: f64 = 0.240_846_7;
-const VENUS_ORBITAL_PERIOD: f64 = 0.615_197_26;
-const MARS_ORBITAL_PERIOD: f64 = 1.880_815_8;
-const JUPITER_ORBITAL_PERIOD: f64 = 11.862_615;
-const SATURN_ORBITAL_PERIOD: f64 = 29.447_498;
-const URANUS_ORBITAL_PERIOD: f64 = 84.016_846;
-const NEPTUNE_ORBITAL_PERIOD: f64 = 164.79132;
-
 #[derive(Debug)]
 pub struct Duration {
     s: u64,
@@ -32,67 +23,29 @@ pub trait Planet {
 }
 
 pub trait OrbitalPeriod {
-    fn orbital_period() -> f64;
+    const ORBITAL_PERIOD: f64; // in Earth years
 }
 
 impl<T: OrbitalPeriod> Planet for T {
     fn years_during(d: &Duration) -> f64 {
-        d.earth_years() / T::orbital_period()
+        d.earth_years() / T::ORBITAL_PERIOD
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl OrbitalPeriod for Mercury {
-    fn orbital_period() -> f64 {
-        MERCURY_ORBITAL_PERIOD
-    }
+macro_rules! define_planet {
+    ($name:ident, $orbital_period:expr) => (
+        pub struct $name;
+        impl OrbitalPeriod for $name {
+            const ORBITAL_PERIOD: f64 = $orbital_period;
+        }
+    );
 }
 
-impl OrbitalPeriod for Venus {
-    fn orbital_period() -> f64 {
-        VENUS_ORBITAL_PERIOD
-    }
-}
-
-impl OrbitalPeriod for Earth {
-    fn orbital_period() -> f64 {
-        1.0
-    }
-}
-impl OrbitalPeriod for Mars {
-    fn orbital_period() -> f64 {
-        MARS_ORBITAL_PERIOD
-    }
-}
-
-impl OrbitalPeriod for Jupiter {
-    fn orbital_period() -> f64 {
-        JUPITER_ORBITAL_PERIOD
-    }
-}
-
-impl OrbitalPeriod for Saturn {
-    fn orbital_period() -> f64 {
-        SATURN_ORBITAL_PERIOD
-    }
-}
-
-impl OrbitalPeriod for Uranus {
-    fn orbital_period() -> f64 {
-        URANUS_ORBITAL_PERIOD
-    }
-}
-
-impl OrbitalPeriod for Neptune {
-    fn orbital_period() -> f64 {
-        NEPTUNE_ORBITAL_PERIOD
-    }
-}
+define_planet!(Mercury, 0.240_846_7);
+define_planet!(Venus, 0.615_197_26);
+define_planet!(Earth, 1.0);
+define_planet!(Mars, 1.880_815_8);
+define_planet!(Jupiter, 11.862_615);
+define_planet!(Saturn, 29.447_498);
+define_planet!(Uranus, 84.016_846);
+define_planet!(Neptune, 164.79132);
