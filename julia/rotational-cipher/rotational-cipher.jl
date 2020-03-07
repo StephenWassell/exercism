37823284fc43d0cd8aaad83e3614e476f6413108
Stephen Wassell <stephen.wassell@sophos.com>
2020-03-07 21:39:38 +0000

    rotational cipher julia

--- julia/rotational-cipher/rotational-cipher.jl
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -1,1 +1,15 @@
-rotate(n, str) = str
+function rotate(n, c::AbstractChar)
+    if 'A' <= c <= 'Z'
+        first = 'A'
+    elseif 'a' <= c <= 'z'
+        first = 'a'
+    else
+        return c
+    end
+
+    (c + n - first) % 26 + first
+end
+
+function rotate(n, str::String)
+    String(map(c -> rotate(n, c), collect(str)))
+end
